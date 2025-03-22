3035a0bd18e6df5f219a6adb78476bf5dda454a8
Jaya-Prakadeesh <85389876+Jaya-Prakadeesh@users.noreply.github.com>
2025-03-21 20:40:51 -0700

    Update RunnerFile.java

--- src/test/java/com/zerobank/runners/RunnerFile.java
+++ src/test/java/com/zerobank/runners/RunnerFile.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src\\test\\resources\\features", glue = {"com\\zerobank\\steps","com\\zerobank\\pages"}, 
 tags = 	//("@SuccessfulLogout or @VerifySessionExpiryLogout")
-		"@StatementDownloading"
+		("@AccountSummaryDetails or @NewPayeeModule or @AuthenticationModule")
 ,plugin = {"pretty","html:reports/HTMLReports.html",
 		"json:reports/json-report.json",
 		"junit:reports/junit_report.xml",
